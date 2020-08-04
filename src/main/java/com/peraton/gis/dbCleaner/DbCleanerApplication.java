1c865ca06d8c7f9cbe23282c785328ccf0360d83
slush62 <steve@lushfamily.com>
2020-08-04 08:20:47 -0500

    initial

---
+++ src/main/java/com/peraton/gis/dbCleaner/DbCleanerApplication.java
@@ -0,0 +1,13 @@
+package com.peraton.gis.dbCleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCleanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbCleanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peraton/gis/dbCleaner/DbCleanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peraton.gis.dbCleaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCleanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
